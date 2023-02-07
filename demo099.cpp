dab97eb08c3f2184d6969005906db23a0f2b9887
18866805256 <3407085928@qq.com>
2023-02-07 21:31:41 +0800

    modified:   demo099.cpp

--- demo099.cpp
+++ demo099.cpp
@@ -1,12 +1,65 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-//vector容器中存放自定义数据类型
+// vector容器中存放自定义数据类型
 class Person
 {
-    string 
+public:
+    Person(string name, int age)
+    {
+        this->m_Name = name;
+        this->m_Age = age;
+    }
+    string m_Name;
+    int m_Age;
+};
+void test01()
+{
+    vector<Person> v;
+    Person p1("a", 10);
+    Person p2("b", 20);
+    Person p3("c", 30);
+    Person p4("d", 40);
+    Person p5("e", 50);
+    // 向容器中添加数据
+    v.push_back(p1);
+    v.push_back(p2);
+    v.push_back(p3);
+    v.push_back(p4);
+    v.push_back(p5);
+    // 遍历容器中的数据
+    for (vector<Person>::iterator it = v.begin(); it != v.end(); it++)
+    {
+        cout << "姓名：" << (*it).m_Name << "年龄：" << (*it).m_Age << endl;
+        // 或使用如下方法
+        // cout << "姓名：" << it->m_Name << "年龄：" << it->m_Age << endl;
+    }
+}
+void test02()
+{
+    vector<Person *> v;
+    Person p1("a", 10);
+    Person p2("b", 20);
+    Person p3("c", 30);
+    Person p4("d", 40);
+    Person p5("e", 50);
+    // 向容器中添加数据
+    v.push_back(&p1);
+    v.push_back(&p2);
+    v.push_back(&p3);
+    v.push_back(&p4);
+    v.push_back(&p5);
+    // 遍历容器
+    for (vector<Person *>::iterator it = v.begin(); it != v.end(); it++)
+    {
+        cout << "姓名：" << (*it)->m_Name << "年龄：" << (*it)->m_Age << endl;
+    }
 }
 int main()
 {
+    test01();
+    cout << "----------" << endl;
+    test02();
     system("pause");
     return 0;
 }
